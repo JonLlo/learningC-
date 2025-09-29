a9d1ef2d860bafc0d168004b2dd583878021773a
Jonny Lloyd <jonnylloyd@Jonnys-MacBook-Air.local>
2025-09-29 14:05:25 +0100

    .

---
+++ TestProject/AddLogicToC/Program.cs
@@ -0,0 +1,36 @@
+﻿//1. Evaluate Boolean Expressions to Make Decisions in C#
+
+
+
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
+
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+
+//conditional operator
+//<evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+//Code challenge: write code to display the result of a coin flip
+
+
+Random coin = new Random() ;
+int roll1 = dice.Next();    
