494c413a53586af3449df6dcbe63748643e92992
Jonny Lloyd <jonnylloyd@Jonnys-MacBook-Air.local>
2025-10-01 14:04:48 +0100

    .

---
+++ TestProject/WorkWithVar/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//2 Discover value types and reference types
+//decimal to binary: 116 -> 01110100
+//text to binary: a = 97, b = 98, A = 65, B =66 -> same process
+//number of bits allow up to 2^n -1. e.g. 128 is 8 bits so allows 0-255
+
+
+//Exercise - Discover integral types
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
