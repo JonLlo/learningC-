5719aa408dca7f5e43f6f4951f7c0f4c1fb4ce75
Jonny Lloyd <jonnylloydie2@gmail.com>
2025-10-03 12:54:22 +0100

    .

---
+++ TestProject/DebugC/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+
+int studentCount = students.Length -1;
+Console.WriteLine(studentCount);
+
+Console.WriteLine("The final name is: " + students[studentCount]);
+
+/*The Run menu provides menu options that are grouped into six sections.
+
+
+1. Start and stop applications: This section of the menu includes options for starting and stopping code execution, with and without the debugger attached.
+
+2. Launch configurations: This section of the menu provides access to examine or create launch configurations.
+
+3. Runtime control: This section of the menu enables the developer to control how they want to advance through the code. Controls are enabled when execution has paused during a debug session.
+
+4. Set Breakpoints: This section of the menu enables the developer to set breakpoints on code lines. Code execution pauses on Breakpoints during a debug session.
+
+5. Manage Breakpoints: This section of the menu enables the developer to manage breakpoints in bulk rather than individually.
+
+6. Install Debuggers: This section of the menu opens the Visual Studio Code EXTENSIONS view filtered for code debuggers.
+/*/
+
+//hi
